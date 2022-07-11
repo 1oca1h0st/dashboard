b9baedd54b4367788125922b64a69ada94bb3ce8
loca1h0st <chenjh.network@gmail.com>
2022-07-11 17:57:21 +0800

    add jinja2 templates

--- app/app.py
+++ app/app.py
@@ -1,8 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+templates = Jinja2Templates(directory="app/templates")
 
 
-@app.get("/")
-async def index():
-    return {"message": "Hello World!"}
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
