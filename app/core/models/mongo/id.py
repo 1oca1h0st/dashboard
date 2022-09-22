041bdd4bced1ed7101995a8f58b1a88f64e6bc67
1oca1h0st <chenjh.network@gmail.com>
2022-09-22 15:22:07 +0800

    PyObjectId bug fix

--- app/core/models/mongo/id.py
+++ app/core/models/mongo/id.py
@@ -14,4 +14,4 @@ class PyObjectId(ObjectId):
 
     @classmethod
     def __modify_schema__(cls, field_schema):
-        field_schema.aupdate(type="string")
+        field_schema.update(type="string")
