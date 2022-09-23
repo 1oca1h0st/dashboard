c171a632b60528a8fcf6db5d32633ed856212902
1oca1h0st <chenjh.network@gmail.com>
2022-09-23 10:13:50 +0800

    添加tags说明

--- app/core/routes/web.py
+++ app/core/routes/web.py
@@ -47,12 +47,12 @@ async def view_login(request: Request):
     return templates.TemplateResponse("auth/login.html", {"request": request})
 
 
-@router.get("/db/{t_id}", response_model=schemas.DemoBase)
+@router.get("/db/{t_id}", response_model=schemas.DemoBase, tags=["mysql"])
 def get_demo_by_id(t_id: int, db: Session = Depends(get_db)):
     return get_demo(db, t_id)
 
 
-@router.get("/mongo/list/{d_id}", response_model=DemoModel)
+@router.get("/mongo/list/{d_id}", response_model=DemoModel, tags=["mongo"])
 async def mongo_list_by_id(d_id: str):
     if (demo := await mongo["demo"].find_one({"_id": d_id})) is not None:
         return demo
@@ -60,13 +60,13 @@ async def mongo_list_by_id(d_id: str):
     raise HTTPException(status_code=404, detail="Demo {} not found!".format(d_id))
 
 
-@router.get("/mongo/list", response_model=List[core.schemas.mongo.DemoBase])
+@router.get("/mongo/list", response_model=List[core.schemas.mongo.DemoBase], tags=["mongo"])
 async def mongo_list_by_id():
     results = await mongo["demo"].find().to_list(1000)
     return results
 
 
-@router.post("/mongo/add", response_model=DemoModel)
+@router.post("/mongo/add", response_model=DemoModel, tags=["mongo"])
 async def mongo_add_by_id(demo: DemoModel = Body(...)):
     demo = jsonable_encoder(demo)
     new_demo = await mongo["demo"].insert_one(demo)
@@ -74,27 +74,27 @@ async def mongo_add_by_id(demo: DemoModel = Body(...)):
     return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_demo)
 
 
-@router.post("/validate")
+@router.post("/validate", tags=["validate"])
 def test_validate(data: DemoRequests):
     return {"msg": "ok"}
 
 
-@router.get("/cache")
+@router.get("/cache", tags=["cache"])
 def get_cache():
     return {"msg": settings.TEST_ENV}
 
 
-@router.get("/celery/{a}/{b}")
+@router.get("/celery/{a}/{b}", tags=["celery"])
 def test_celery(a: int, b: int):
     add_job = add.delay(a, b)
     print(add_job)
     return {"id": str(add_job)}
 
 
-@router.get("/celery/{task_id}")
+@router.get("/celery/{task_id}", tags=["celery"])
 def get_celery_task_by_id(task_id: str):
     print(add.AsyncResult(task_id).status)
 
 
-router.include_router(test.router, prefix="/test")
-router.include_router(users.router, prefix="/users")
+router.include_router(test.router, prefix="/test", tags=["test"])
+router.include_router(users.router, prefix="/users", tags=["users"])
